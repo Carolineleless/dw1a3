c8a816f11dd9289f9330844d24ec4846637c0ed9
Carolineleless <101575642+Carolineleless@users.noreply.github.com>
2023-05-21 20:14:44 -0300

    Delete Atividades/A9 directory

--- Atividades/A9/script.js
+++
@@ -1,6 +0,0 @@
-function updatePreview() {
-  var opacityValue = document.getElementById('code-input').value;
-  var preview = document.getElementById('result-preview');
-  var img = preview.querySelector('img');
-  img.style.opacity = opacityValue;
-}
